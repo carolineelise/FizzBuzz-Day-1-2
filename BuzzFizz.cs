9a468eddd146880275bbf6e5e25deb6f03b1da77
carolineelise <90719451+carolineelise@users.noreply.github.com>
2021-12-14 15:06:15 -0500

    Add files via upload

---
+++ BuzzFizz.cs
@@ -0,0 +1,31 @@
+﻿#define XFrench
+using System;
+
+namespace Day_1._5_cont_starting_FizzBuzz
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("False. Bears. Beets. Battlestar Galactica.");
+            //{ for (var idx = 1; idx <= 30; idx++) { Console.Write($"FizzBuzz ")};
+            //  if (idx % 3 == 0) { Console.Write("Fizz")} else; 
+            //if (idx % 5 == 0) { Console.Write("Buzz")} else;
+            //{ Console.Write("{idx}")};
+            //}
+
+            var yes = "Yes";
+            var no = "No";
+#if French
+            var yes = "Oui";
+            var no = "Non";
+#endif
+            var boolean = true;
+            Console.WriteLine($"Boolean is {boolean}; text is {(boolean ? yes : no)}");
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day_1._5_cont_starting_FizzBuzz
+{
+    class Program
+    {
+        private static int idx;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("False. Bears. Beets. Battlestar Galactica.");
+            for (var idx = 1; idx <= 30; idx++) { Console.Write($"FizzBuzz ")};
+            else if (idx % 3 == 0) { Console.Write("Fizz")};
+            else if (idx % 5 == 0) { Console.Write("Buzz")};
+                else {  Console.Write("{idx}")};
+
+
+        }
+    }
+}
